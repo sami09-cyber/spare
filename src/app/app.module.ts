35cef208313886ca69242bee45fef4b2560ca99a
sami <bagaassami009@gmail.com>
2025-01-16 10:48:03 +0100

    intialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"spare-89438","appId":"1:338478839189:web:e8affe86803fe8873c329c","storageBucket":"spare-89438.firebasestorage.app","apiKey":"AIzaSyBgjHauiBVCwfHZMuar25Z7EXjQYhmX-Ns","authDomain":"spare-89438.firebaseapp.com","messagingSenderId":"338478839189","measurementId":"G-N7Q2DYX5FK"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
